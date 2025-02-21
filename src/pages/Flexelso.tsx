1c2db9d662f750446dbccac3222b2c5073078984
Táborosi Máté Kristóf <78024133+Zigoisten@users.noreply.github.com>
2025-02-21 14:20:50 +0100

    Add files via upload

---
+++ src/pages/Flexelso.tsx
@@ -0,0 +1,12 @@
+import "./elso.css";
+const Flexelso = () =>{
+    return <body>
+        <ul className="flex-container">
+            <li id="item"></li>
+            <li id="item"></li>
+            <li id="item"></li>
+            <li id="item"></li>
+        </ul>
+    </body>
+}
+export default Flexelso
