3ba8592ca89657c927cc721f35cdef18843fe5be
Táborosi Máté Kristóf <78024133+Zigoisten@users.noreply.github.com>
2025-02-24 09:41:05 +0100

    Add files via upload

---
+++ Labrador/src/pages/Labrador.tsx
@@ -0,0 +1,20 @@
+import "./Stilus.css";
+
+function Labrador() {
+    return (
+        <div>
+            <div className="card">
+                <div className="main">
+                    <div className="title"><h2>Labrador</h2></div>
+                    <h1><b>$15</b>/mo</h1>
+                    <p>Unlimited Matches</p>
+                    <p>Unlimited Messages</p>
+                    <p>Unlimited App Usage</p>
+                    <button>Get Started</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Labrador;
